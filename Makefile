65cdde79be9b8483d5e71453f1e68a74326c9a56
donus3 <donus33@gmail.com>
2019-10-18 10:32:02 +0700

    update: made variable value in the config

--- Makefile
+++ Makefile
@@ -1,11 +1,21 @@
 OS_NAME := $(shell uname -s | tr A-Z a-z)
 
+.PHONY: all
+.PHONY: build
 .PHONY: run
+.PHONY: stop
+
+all: build run
+
+build:
+	docker-compose build
 
 run:
 ifeq ($(OS_NAME), linux-gnu)
-	HOST=localhost docker-compose up --build
+	HOST=localhost docker-compose up -d
 else
-	HOST=host.docker.internal docker-compose up --build
+	HOST=host.docker.internal docker-compose up -d
 endif
 
+stop:
+	docker-compose down
--- scripts/grpcwebproxy.sh
+++ scripts/grpcwebproxy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-echo "HOST = $HOST"
-sed -e "s/%HOST_ADDRESS%/$HOST/g" /config.yaml > /etc/envoy/envoy.yaml
-cat /etc/envoy/envoy.yaml
-eval "/usr/local/bin/envoy -c /etc/envoy/envoy.yaml"
+sed -i "s/%HOST_ADDRESS%/$HOST/g" /config.yaml
+sed -i "s/%ADMIN_PORT%/$ADMIN_PORT/g" /config.yaml
+sed -i "s/%LISTENER_PORT%/$LISTENER_PORT/g" /config.yaml
+sed -i "s/%HOST_PORT%/$HOST_PORT/g" /config.yaml
+
+eval "/usr/local/bin/envoy -c /config.yaml"
